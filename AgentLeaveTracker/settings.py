a2a7753a2d4db7f5b33b6745a30a42be375b6123
WarMiner <warminer001@gmail.com>
2020-04-19 21:21:20 +0800

    Added

--- AgentLeaveTracker/settings.py
+++ AgentLeaveTracker/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
